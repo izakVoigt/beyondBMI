6412b51b8069f3417963816a77bfd79e1bd09c5b
Izak Voigt <izak.voigt@gmail.com>
2025-12-14 11:20:19 +0000

    feat(Api): :lock: implement basic security modules

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,6 @@
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
 
 import { env } from './app/configs/envs/env';
 import { httpLogger } from './app/configs/logger/http-logger';
@@ -8,10 +10,18 @@ const { PORT } = env;
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: false }));
 app.use(httpLogger);
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
+});
+
+app.use((_req, res) => {
+  res.status(400).json({ message: 'Route not found' });
 });
 
 const server = app.listen(PORT, () => {
