5d8cc9a6ee6b828db95f00fd21923b5bdc64ce82
Izak Voigt <izak.voigt@gmail.com>
2025-12-15 21:38:21 +0000

    chore(Lib Common): :wrench: update jest config var name

--- libs/common/jest.unit.config.ts
+++ libs/common/jest.unit.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from 'jest';
 
-const testConfigs: Config = {
+const unitTestConfigs: Config = {
   coveragePathIgnorePatterns: ['<rootDir>/src/enums/'],
   displayName: 'lib-common',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
 };
 
-export default testConfigs;
+export default unitTestConfigs;
